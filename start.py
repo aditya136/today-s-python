76cbda47c2add9028fd78dd8f171c0b7bbb54508
aditya136 <adityabanerjee102@gmail.com>
2023-01-23 23:25:54 -0800

    comments in Python

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
-#Pritom dada is learning about Python...
+#Are you learning Python?
 #tere is a thing called variable
-It_is_a_variable = "and_it_is_a_string_or_the_value_of_the_variable"
+It_is_a_variable = "and_it_is_a_string_or_the_value_of_the_variable"#We can write comments like this.
 #A common function that give back the thing wihch we writen in this function called print() function
 print(It_is_a_variable)
+#Break a leg
