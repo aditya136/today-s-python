96459a07ab0f2784b5b52567caca6baa2b0c7582
aditya136 <adityabanerjee102@gmail.com>
2023-01-25 23:44:27 -0800

    Strings_in_Python

--- start.py
+++ start.py
@@ -1,6 +1,12 @@
-#Are you learning Python?
-#tere is a thing called variable
-It_is_a_variable = "and_it_is_a_string_or_the_value_of_the_variable"#We can write comments like this.
-#A common function that give back the thing wihch we writen in this function called print() function
-print(It_is_a_variable)
-#Break a leg
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
